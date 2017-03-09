80dde1e27f9830733efce8096da41df7f146b0ae
Vladimir Sukhov <vladimir.sukhov@atomicus.by>
2017-03-09 12:00:31 +0300

    u

--- NavigationUpdate/NavigationUpdate.Tests/AddUserTest.cs
+++ NavigationUpdate/NavigationUpdate.Tests/AddUserTest.cs
@@ -14,17 +14,19 @@ namespace NavigationUpdate.Tests
         {
             var context = new MyDbContext();
             //context.Usergroups.Add(
-            ////    new Usergroup() {
-            ////        Name = "Group1"
-            ////    }
-            ////    );
-            ////context.Permissions.Add(
-            ////    new Permission()
-            ////    {
-            ////        Name = "Permission1"
-            ////    }
-            ////    );
-            var usergroup = context.Usergroups.FirstOrDefault();
+            //    new Usergroup()
+            //    {
+            //        Name = "Group1"
+            //    }
+            //    );
+            //context.Permissions.Add(
+            //    new Permission()
+            //    {
+            //        Name = "Permission1"
+            //    }
+            //    );
+            //context.SaveChanges();
+            var usergroup = context.Set<Usergroup>().FirstOrDefault();
             usergroup.Name = "bla-bla";
             usergroup.Permissions = new Collection<Permission>()
             {
@@ -35,6 +37,35 @@ namespace NavigationUpdate.Tests
                      Name="test2"
                 }
             };
+            context.SaveChanges();
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var context = new MyDbContext();
+            
+            var usergroup = context.Set<Usergroup>().FirstOrDefault();
+            usergroup.Name = "bla-bla";
+            var perm = usergroup.Permissions;
+            var item = new Permission()
+            {
+                Name = "test1"
+            };
+            usergroup.Permissions.Add(item);
+            context.SaveChanges();
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var context = new MyDbContext();
+
+            var usergroup = context.Set<Usergroup>().FirstOrDefault();
+            usergroup.Name = "bla-bla";
+            var perm = usergroup.Permissions.First();
+            context.Set<Permission>().Remove(perm);
             context.SaveChanges();
         }
     }
